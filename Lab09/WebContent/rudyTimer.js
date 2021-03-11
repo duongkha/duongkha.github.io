502fb8944751c97c2eaa9fded2b5f56207e41d5e
Kha Nguyen <dknguyen@miu.edu>
2021-03-11 12:50:50 -0600

    add

---
+++ Lab09/WebContent/rudyTimer.js
@@ -0,0 +1,16 @@
+var rudyTimer = (function(){
+	timer = null ;// stores ID of interval timer
+	function delayMsg2(){
+		if (timer === null)
+		{
+			timer = setInterval(rudy, 1000);
+		}else{
+			clearInterval(timer);
+			timer = null;
+		}
+	}
+	function rudy(){
+		document.getElementById("output").innerHTML += "Rudy!";
+	}
+	delayMsg2();
+});
